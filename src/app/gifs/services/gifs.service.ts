67f6cb79c1cbae1b14ebcbea4139c5bbafc670ea
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 01:35:24 -0500

    Se agrego la conexión con el Api de Giphy

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,13 +6,21 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private _apiKey: string = '31eGRaf5mW32M2qpJ5cctd8Gcqy5WY4B'
  
   private _historial: string[]=[];
+
+  //TODO: Cambiar any por su tipo correspondiente
+  public resultados : any[]=[];
+
 
   get historial (){
     return [...this._historial];
   }
   
+  constructor(private http:HttpClient){
+
+  }
   buscarGifs(query:string=''){
     query=query.trim().toLowerCase();
     if(query.trim().length==0)return;
@@ -19,7 +28,13 @@ export class GifsService {
     if(!this._historial.includes(query)) this._historial.unshift(query);
     this._historial = this._historial.splice(0,10);
 
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=10`)
+      .subscribe( (resp:any) =>{
+        console.log(resp.data);
+        this.resultados= resp.data;
+      });
+  
+
   }
 
 
