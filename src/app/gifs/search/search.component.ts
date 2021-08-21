98f993f856bdfd1e8aeb72d792b9d71e5f37ffb6
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 00:33:57 -0500

    Se agregó el servicio para el historial de busquedas, así como optimizaciónes al mismo

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styles: [
-  ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  //El signo de ! es el not null assertion operator lo cual nos permitira decirle a
+  //typescript que ese valor nunca sera null
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService :GifsService){
 
-  ngOnInit(): void {
   }
 
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value='';
+    
+  }
 }
