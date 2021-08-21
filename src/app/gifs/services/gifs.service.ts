bf7b5df91975dd10fa83fc197aa822dec81f3575
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-21 02:10:00 -0500

    Se agregó la persistencía de las busquedas e historial utilizando local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -20,21 +20,32 @@ export class GifsService {
   }
   
   constructor(private http:HttpClient){
-
+    //En caso de ser null el valor en el local storage nos regresara un arreglo vacio en lugar de un nulls
+    this._historial=JSON.parse(localStorage.getItem('historial')!)|| [];
+    this.resultados=JSON.parse(localStorage.getItem('ultimaBusqueda')!)|| [];
+    
   }
+  //Metodo que nos permite consumir el servicio de la API de giphy
+  //para obtener los giphy correspondientes al query
   buscarGifs(query:string=''){
     query=query.trim().toLowerCase();
+    //Si viene vacio el query lanza un return
     if(query.trim().length==0)return;
     
+    //Metodo para validar que no este se ingresen duplicados al historial
     if(!this._historial.includes(query)) this._historial.unshift(query);
     this._historial = this._historial.splice(0,10);
 
+    //Guardar los datos en LocalStorage
+    localStorage.setItem('historial', JSON.stringify(this._historial));
+    
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=10`)
-      .subscribe( (resp) =>{
-        console.log(resp.data);
-        this.resultados= resp.data;
-      });
-  
+    .subscribe( (resp) =>{
+      console.log(resp.data);
+      this.resultados= resp.data;
+      localStorage.setItem('ultimaBusqueda', JSON.stringify(this.resultados));
+    });
+    
 
   }
 
